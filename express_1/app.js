6b54af4750386f9d68c78ae0e9530ced6fcfd442
eilinge <17601329166@163.com>
2019-03-29 14:34:54 +0800

    new ejs study

--- express_1/app.js
+++ express_1/app.js
@@ -4,8 +4,8 @@ var app = express();
 // fetch test
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-    res.render("news", {
-        "news": ["新闻1", "新闻2", "新闻3"],
-        "new1": ["新闻2", "新闻2", "新闻3"]
+    res.render("test_ejs", {
+        title: "Cleaning Supplies",
+        supplies: ["mop", "broom", "duster"]
     })
 }).listen(3000);
